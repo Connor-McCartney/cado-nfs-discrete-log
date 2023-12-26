98006758dce0f237f7e3f4a53debf4654ec700e3
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-12-27 03:41:18 +1000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,12 @@
-#todo
+from cado_nfs_helper import n_order, pohlig_hellman
+from random import randint
+
+def test1():
+    p = 33184772290615481426295675425316668758122179640330548849957081783509
+    g = 5
+    order, f = n_order(g, p, isprime=True)
+    d = randint(0, order)
+    a = pow(g, d, p)
+    print(d == pohlig_hellman(p, a, g, order, f))
+
+test1()
