fabf72da8b18959b86b6c0ccaeb027abc8cd5639
Connor-McCartney <89777327+Connor-McCartney@users.noreply.github.com>
2023-12-27 03:53:33 +1000

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ from cado_nfs_helper import n_order, pohlig_hellman
 from random import randint
 
 def test1():
+    # ~15 mins
     p = 33184772290615481426295675425316668758122179640330548849957081783509
     g = 5
     order, f = n_order(g, p, isprime=True)
